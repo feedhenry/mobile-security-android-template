30db7f28d042972b8dede1e39c9fbd108ebb1932
Wei Li <wei.li@redhat.com>
2017-09-15 12:42:33 +0100

    🎨 add tags for fragments transactions

--- app/src/main/java/com/feedhenry/securenativeandroidtemplate/navigation/Navigator.java
+++ app/src/main/java/com/feedhenry/securenativeandroidtemplate/navigation/Navigator.java
@@ -2,6 +2,7 @@ package com.feedhenry.securenativeandroidtemplate.navigation;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.feedhenry.securenativeandroidtemplate.BaseActivity;
