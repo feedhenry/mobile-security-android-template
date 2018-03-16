14a1bb21827c6bbd0fcfd62b1c6a5b716db9b0c4
Wei Li <wei.li@redhat.com>
2018-03-16 12:16:46 +0000

    make sure the dialog window won't be dismissed

--- app/src/main/java/com/feedhenry/securenativeandroidtemplate/features/device/views/WarningDialog.java
+++ app/src/main/java/com/feedhenry/securenativeandroidtemplate/features/device/views/WarningDialog.java
@@ -5,17 +5,28 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.feedhenry.securenativeandroidtemplate.R;
 
 public class WarningDialog extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        getDialog().setCanceledOnTouchOutside(false);
+        return view;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-
         builder.setCustomTitle(inflater.inflate(R.layout.fragment_warning_title, null));
         builder.setMessage(R.string.device_trust_warning)
                 .setPositiveButton(R.string.device_continue, new DialogInterface.OnClickListener() {
